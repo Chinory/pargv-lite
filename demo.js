6362d138e231e1d4b7a9f2db9b4b823741599341
chinory <32269068+chinory@users.noreply.github.com>
2018-07-20 00:21:14 +0800

    support mode option

--- demo.js
+++ demo.js
@@ -5,13 +5,30 @@ const BASENAME = require('path').basename(process.argv[1])
 const opts = (() => {
   const parseArgv = require('.')
   const options = {
-    action: { def: true, use: ['n', 'dry-run'] },
-    verbose: { def: false, use: ['v', 'verbose'] },
-    mode: { def: 'default', use: ['m', 'mode'] },
-    includes: { def: [], use: ['i', 'include'] },
-    unopened_option: { def: false, use: [] },
-    help: { def: false, use: ['h', 'help'] },
-    version: { def: false, use: ['version'] }
+    action: { use: ['n', 'dry-run'], def: true },
+    verbose: { use: ['v', 'verbose'], def: false },
+    mode: { use: ['m', 'mode'], def: 'default' },
+    includes: { use: ['i', 'include'], def: [] },
+    unopened_option: { use: [], def: false },
+    help: { use: ['h', 'help'], def: false },
+    version: { use: ['version'], def: false },
+    rm: { use: ['r', 'rm'], def: {
+      force: { use: ['f', 'force'], def: false },
+      prompt: { use: ['i'], def: false },
+      prompt_once: { use: ['I'], def: false },
+      interactive: { use: ['interactive'], def: 'always' },
+      one_file_system: { use: ['one-file-system'], def: false },
+      preserve_root: { use: ['no-preserve-root'], def: true },
+      recursive: { use: ['r', 'R', 'recursive'], def: false },
+      remove_empty_dir: { use: ['d', 'dir'], def: false },
+      verbose: { use: ['v', 'verbose'], def: false },
+      help: { use: ['h', 'help'], def: false },
+      dev: { use: ['dev'], def: {
+        action: { use: ['n', 'dry-run'], def: true },
+        verbose: { use: ['v', 'verbose'], def: false },
+        help: { use: ['h', 'help'], def: false },
+      }}
+    }}
   }
   try {
     return parseArgv(process.argv.slice(2), options)
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-module.exports = function parseArgv (argv, options) {
+module.exports = function parseArgv (argv, options, modePath = '') {
   const opts = {_: []}
   const names = {}
   for (const opt in options) {
@@ -33,7 +33,7 @@ module.exports = function parseArgv (argv, options) {
           }
         } else {
           if (optNeedArg) {
-            throw new Error(`missing option argument -- ${nameNeedArg}`)
+            throw new Error(`${modePath}missing option argument -- ${nameNeedArg}`)
           }
           const eq = cur.indexOf('=', 2)
           if (eq === -1) {
@@ -42,12 +42,18 @@ module.exports = function parseArgv (argv, options) {
             if (opt) {
               if (typeof options[opt].def === 'boolean') {
                 opts[opt] = !options[opt].def
+              } else if (options[opt].def instanceof Array) {
+                optNeedArg = opt
+                nameNeedArg = name
+              } else if (options[opt].def instanceof Object) {
+                opts[opt] = parseArgv(argv.slice(i + 1), options[opt].def, modePath + `${name}: `)
+                return opts
               } else {
                 optNeedArg = opt
                 nameNeedArg = name
               }
             } else {
-              throw new Error(`invalid option -- ${name}`)
+              throw new Error(`${modePath}invalid option -- ${name}`)
             }
           } else {
             const name = cur.slice(2, eq)
@@ -57,11 +63,13 @@ module.exports = function parseArgv (argv, options) {
                 opts[opt] = !options[opt].def
               } else if (options[opt].def instanceof Array) {
                 opts[opt].push(cur.slice(eq + 1))
+              } else if (options[opt].def instanceof Object) {
+                throw new Error(`${modePath}mode option syntax error -- ${name}`)
               } else {
                 opts[opt] = cur.slice(eq + 1)
               }
             } else {
-              throw new Error(`invalid option -- ${name}`)
+              throw new Error(`${modePath}invalid option -- ${name}`)
             }
           }
         }
@@ -79,7 +87,7 @@ module.exports = function parseArgv (argv, options) {
           }
         } else {
           if (optNeedArg) {
-            throw new Error(`missing option argument -- ${nameNeedArg}`)
+            throw new Error(`${modePath}missing option argument -- ${nameNeedArg}`)
           }
           let last = cur.length - 1
           for (let j = 1; j < last; ++j) {
@@ -91,6 +99,8 @@ module.exports = function parseArgv (argv, options) {
               } else {
                 if (options[opt].def instanceof Array) {
                   opts[opt].push(cur.slice(j + 1))
+                } else if (options[opt].def instanceof Object) {
+                  throw new Error(`${modePath}mode option syntax error -- ${name}`)
                 } else {
                   opts[opt] = cur.slice(j + 1)
                 }
@@ -98,7 +108,7 @@ module.exports = function parseArgv (argv, options) {
                 break
               }
             } else {
-              throw new Error(`invalid option -- ${name}`)
+              throw new Error(`${modePath}invalid option -- ${name}`)
             }
           }
           if (last) {
@@ -107,12 +117,15 @@ module.exports = function parseArgv (argv, options) {
             if (opt) {
               if (typeof options[opt].def === 'boolean') {
                 opts[opt] = !options[opt].def
+              } else if (options[opt].def instanceof Object) {
+                opts[opt] = parseArgv(argv.slice(i + 1), options[opt].def, modePath + `${name}: `)
+                return opts
               } else {
                 optNeedArg = opt
                 nameNeedArg = name
               }
             } else {
-              throw new Error(`invalid option -- ${name}`)
+              throw new Error(`${modePath}invalid option -- ${name}`)
             }
           }
         }
@@ -131,7 +144,7 @@ module.exports = function parseArgv (argv, options) {
     }
   }
   if (optNeedArg) {
-    throw new Error(`missing option argument -- ${nameNeedArg}`)
+    throw new Error(`${modePath}missing option argument -- ${nameNeedArg}`)
   }
   for (const _ = opts._; i < argv.length; ++i) {
     _.push(argv[i])
