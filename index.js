b66ac0235146871ea3c015ad61a1cd0982a56090
Chinory <chinory@cup.moe>
2025-04-04 18:07:31 +0800

    v3.1: add Halt Option

--- index.js
+++ index.js
@@ -1,174 +1,197 @@
 'use strict';
-const isA = Array.isArray;
-const isB = x => typeof x === 'boolean';
+const isA = Array.isArray,
+	isB = x => typeof x === 'boolean';
 /**
- * @typedef {(boolean | string | string[])} VarVal Variable value
- * @typedef {string} VarKey Variable name, just a string
+ * @typedef {string} VarKey Variable name
+ * @typedef {string | boolean | string[]} VarVal Variable value
  * @typedef {string} OptStr Option string, e.g. '--', '-o', '--option'
- * @typedef {Record<OptStr, VarKey>} OptToVarMap
+ * @typedef {OptStr | null} OptDef Option definitions, e.g. '--', '-o', '--option', or `null` to refer to the variable name
+ * @typedef {OptDef | OptDef[]} OptKit one or more option definitions
  * 
  * @typedef {object} VarKit Variable configuration object
- * @property {VarVal} def Default value and the variable definition (pun intended)
- * @property {OptStr[]} [set] Array of options to set the variable value
- * @property {OptStr[]} [rst] Array of options to reset the variable value
+ * @property {VarVal} def Variable **def**inition & **def**ault value (pun intended)
+ * @property {OptKit} [set] Array of options to set the variable value
+ * @property {OptKit} [rst] Array of options to reset the variable value
  * 
- * @typedef {Record<VarKey, VarKit>} VarKitMap Mapping of variable names to their configurations
+ * @typedef {OptKit} HaltKit Halt options, identical to `OptKit`, for now...
+ * @typedef {{opt: OptStr, key: VarKey}} HaltRes
+ * @typedef {Record<VarKey, VarKit | HaltKit>} KeyKitMap
+ * @typedef {Record<VarKey, VarVal>} KeyValMap
  * 
  * @callback CanQuit
- * @param {string} err The error message to handle
- * @param {string} opt The related option that caused the error
+ * @param {{msg: string, i: number, opt: OptStr, key?: VarKey, val?: VarVal }} err
  * @returns {boolean} Whether the parsing should continue (false) or quit (true)
+ * @typedef {Record<OptStr, VarKey>} OptKeyMap internal type
  */
 /**
  * Command line argument parser function
- * 
  * @param {string[]} argv Command line arguments array
  * @param {number} i Index of current argument being processed
- * @param {VarKitMap} vkm Options structure definition
- * @param {object} res Object to store parsed results
- * @param {CanQuit} ask Error handler function
- * @returns {void}
- * 
+ * @param {KeyKitMap} req Options structure definition
+ * @param {KeyValMap} res Object to store parsed results
+ * @param {CanQuit} err Error handler function
+ * @returns {{ i: number, halt?: HaltRes }}
  * @example
- * const res = {};
- * parse(process.argv, 2, {
- *   help: {
- *     def: false,
- *     set: ['-h', '--help']
- *   }
- * }, res, (err, opt) => {
- *   console.error(`Error: ${err} (option: ${opt})`);
- *   return true; // Quit parsing
- * });
- * 
- * @warning This function may modify `argv` for convenient. 
  */
-export default function parse(argv, i, vkm, res, ask) {
-	/** @type {OptToVarMap} options to call set */
+export default function parse(argv, i, req, res, err) {
+	/** @type {OptKeyMap} options to set var */
 	const set_ = {};
-	/** @type {OptToVarMap} options to call reset */
+	/** @type {OptKeyMap} options to reset var */
 	const rst_ = {};
+	/** @type {OptKeyMap} options to halt parse */
+	const hlt_ = {};
 	/** @type {OptStr} option */
-	let opt;
-	/** @type {OptStr} option of an extension */
-	let ext;
-	/** @type {VarKey} key */
+	let opt = '';
+	/** @type {OptStr | null} option of an extension */
+	let ext = null;
+	/** @type {VarKey | undefined} key (because of using object...) */
 	let key;
-	/** @type {VarKey} key of anonymous variable */
+	/** @type {VarKey | undefined} key of anonymous variable */
 	let ann;
-
+	// methods
+	const ask = (msg, val) => err({msg, i, opt, key, val});
+	// assert(key != null)
 	const set = val => {
-		const arr = res[key];
-		if (isA(arr)) arr.push(val); else res[key] = val;
+		const cur = res[key];
+		if (isA(cur)) cur.push(val); else res[key] = val;
 	};
 	const rst = () => {
-		const def = vkm[key].def;
+		const def = req[key].def;
 		res[key] = isA(def) ? def.slice() : def;
-	}
-	const nsB = () => {
-		const def = vkm[key].def;
+	};
+	const noB = () => {
+		const def = req[key].def;
 		if (isB(def)) res[key] = !def; else return true;
-	}
-
-	for (key in vkm) {
-		const vk = vkm[key];
-		const def = vk.def;
-		let opts = vk.set;
-		if (isA(def)) {
-			if (opts) for (opt of opts) if (opt !== '--') set_[opt] = key; else ann = key;
-			res[key] = def.slice();
-		} else {
-			if (opts) for (opt of opts) if (opt !== '--') set_[opt] = key;
-			res[key] = def;
+	};
+	// prepare
+	for (key in req) {
+		const vk = req[key];
+		H: {
+			/** @type {HaltKit} */
+			let hk; 
+			if (vk == null) hk = [key];
+			else if (isA(vk)) hk = vk; 
+			else if (typeof vk === 'object') break H;
+			else hk = [vk];
+			for (const od of hk)
+				if (od!=='--') hlt_[od||key] = key;
+			continue;
 		}
-		if (opts = vk.rst) for (opt of opts) if (opt !== '--') rst_[opt] = key;
+		let ok = vk.set;
+		const def = vk.def;
+		if (isA(def)) {
+			res[key] = def.slice();
+			if (ok !== undefined)
+				for (const od of isA(ok)?ok:[ok])
+					if (od!=='--') set_[od||key] = key; else ann = key;
+		} else {
+			res[key] = def;
+			if (ok !== undefined)
+				for (const od of isA(ok)?ok:[ok])
+					if (od!=='--') set_[od||key] = key; // no stomach
+		}
+		if ((ok = vk.rst) !== undefined)
+			for (const od of isA(ok)?ok:[ok]) 
+				if (od!=='--') rst_[od||key] = key;
 	}
-
-	for (; i < argv.length; ++i) I: {
+	// process
+	/** @type {HaltRes} */
+	let halt = null;
+	I: for (; i < argv.length; ++i) {
 		const s = argv[i];
-		// extension
-		if (ext) {
-			const e2 = ext;
-			ext = null;
-			if (key = set_[e2]) {
+		// extension ~ Just bite one more thing
+		if (ext != null) { // fact(const val = s)
+			ext = null; // assert(opt === ext)
+			if (key != null) { // assert(key === set_[opt])
 				set(s);
-				break I;
+				continue I;
 			} else {
-				if (ask('invalid option', e2)) return;
-				// can use s as sth else
+				if (ask('invalid option', s)) break I;
 			}
 		}
-		// ordinary
+		// halt ~ It should be this simple, right?
+		if (key = hlt_[s]) {
+			opt = s;
+			halt = {opt, key};
+			break I;
+		}
+		// ordinary ~ No more dashes!
 		if (s.length < 2 || s[0] !== '-') {
-			if (key = set_[s]) {
-				if (nsB()) ext = s;
-			} else if (key = rst_[s]) {
+			opt = s;
+			if (key = set_[opt]) {
+				if (noB()) ext = opt;
+			} else if (key = rst_[opt]) {
 				rst();
 			} else if (ann) {
-				res[ann].push(s);
+				res[ann].push(opt);
 			} else {
-				if (ask('invalid option', s)) return;
+				if (ask('invalid option')) break I;
 			}
-			break I;
+			continue I;
 		}
-		// ultimate
+		// ultimate ~ Eat you all nom nom
 		if (s === '--') {
-			if (ann) {
-				const arr = res[ann];
-				for (++i; i < argv.length; ++i) arr.push(argv[i]);
-				return;
+			opt = s;
+			if (key = ann) { 
+				const val = res[key];  // assert(isA(val))
+				for (++i; i < argv.length; ++i) val.push(argv[i]);
+				break I;
 			} else {
-				if (ask('unexpected argument', s)) return;
+				if (ask('unexpected argument')) break I;
 			}
-			break I;
+			continue I;
 		}
 		// --opt?val
 		if (s[1] === '-') {
 			const j = s.indexOf('=');
-			// --opt val -> extension
+			// --opt val ~ Make extension
 			if (j < 0) { 
-				ext = s;
-				break I;
+				opt = s;
+				key = set_[opt]; // for the assertion
+				ext = opt;
+				continue I;
 			}
-			// --opt=val
+			// --opt=val ~ Explicit assignment
 			opt = s.slice(0, j);
 			if (key = set_[opt]) {
+				const val = s.slice(j + 1);
 				if (isB(res[key])) {
-					if (ask('Cannot assign a value to a boolean-type option', opt)) return;
+					if (ask('Cannot assign a value to a boolean-type option', val)) break I;
 				} else {
-					set(s.slice(j + 1));
+					set(val);
 				}
-			} else if (rst_[opt]) {
-				if (ask('Cannot assign a value to a reset-type option', opt)) return;
+			} else if (key = rst_[opt]) {
+				if (ask('Cannot assign a value to a reset-type option')) break I;
 			} else {
-				if (ask('invalid option', opt)) return;
+				if (ask('invalid option')) break I;
 			}
-			break I;
+			continue I;
 		} 
-		// ab in -abc123
+		// ab in -abc123 ~ Cannot make extension
 		const J = s.length - 1;
 		for (let j = 1; j < J; ++j) {
 			opt = '-' + s[j];
 			if (key = set_[opt]) {
-				if (nsB()) {
+				if (noB()) {
 					set(s.slice(j + 1));
-					break I;
+					continue I;
 				}
 			} else if (key = rst_[opt]) {
 				rst();
 			} else {
-				if (ask('invalid option', opt)) return;
+				if (ask('invalid option')) break I;
 			}
 		}
-		// c in -abc123
+		// c in -abc123 ~ You can't be annonymous
 		opt = '-' + s[J];
 		if (key = set_[opt]) {
-			if (nsB()) ext = opt;
+			if (noB()) ext = opt;
 		} else if (key = rst_[opt]) {
 			rst();
 		} else {
-			if (ask('invalid option', opt)) return;
+			if (ask('invalid option')) break I;
 		}
 	}
-	if (ext) ask('This option requires an argument', ext);
+	if (ext) ask('This option requires an argument'); // assertion same as above
+	return {i, halt};
 };
