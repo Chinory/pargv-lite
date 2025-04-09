4bc18cf11d46b38ec3de51c3cf58a38416ac308a
Chinory <chinory@cup.moe>
2025-04-09 15:18:54 +0800

    add jsdoc PropKit hell many features

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-const isA = Array.isArray;
 /**
  * @typedef {string} Key Variable name, in most cases, or kit name
  * @typedef {string} Opt Option string, e.g. '--', '-o', '--option'
@@ -9,53 +8,67 @@ const isA = Array.isArray;
  * @typedef {boolean | undefined} Bool actually `Optional<Boolean>`. yes since a bool can't be inside an array in this project, this design is good, very good, PERFECT
  * @typedef {string | number | null} Text Values that can be written as text IN JSON. it's the project POSITIONING
  * @typedef {Text[]} List only adding or resetting for now. oh this is enough as this is just a configurer not a programming language
+ * @typedef {{ [nestable: string]: { bool: boolean, str: string, num: number, list: Text[] } }} Prop supprt `-Xo1.o2.o3 val`, `-Xo1.o2=val` `--opt=o1=val`
  * 
  * @typedef {Object} BoolKit only appearance matters don't give some extensions to it
  * @property {Bool} [_] ***def***: Variable **def**inition & **def**ault value (pun intended)
  * @property {OptKit} [set] Options to set !***def***. Raise `err` if is not a `boolean`. Identical with `not`. // maybe not like this?? any other behavior?
- * @property {OptKit} [rst] Options to set ***def***
- * @property {OptKit} [nul] Options to set `undefined`. (what??) maybe the user wants to cause some NullExceptions
  * @property {OptKit} [yes] Options to set `true`
  * @property {OptKit} [no]  Options to set `false`
  * @property {OptKit} [on]  Options to set `true`
  * @property {OptKit} [off] Options to set `false`
  * @property {OptKit} [not] Options to set !***def***. Raise `err` if is not a `boolean`
  * @property {OptKit} [inv] Options to set `!cur`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [nul] Options to set `undefined`. (what??) maybe the user wants to cause some NullExceptions
+ * @property {OptKit} [rst] Options to set ***def***
  * 
  * @typedef {Object} TextKit you have to use something. but if i say, when ext=0, it is just Bool..hmmmm BUT you can't name a zero size setter, it must be 'set', 'set_set', 'str_num', but not ''(???)
  * @property {Text?} _ ***def***: Variable **def**inition & **def**ault value (pun intended)  can be `null`, maybe this is the only way to provide `Optional<Text>`
  * @property {OptKit} [set] Options to set the raw string slice from the argv
- * @property {OptKit} [rst] Options to set ***def***. The short name is reasonable, so you don't match `reset` when you search `set`, it IS `rst`
- * @property {OptKit} [nul] Options to set `null`. (what??) maybe the user wants to cause some NullExceptions
- * @property {number} [ext] Deprecated (it's in setter name now) consume such number of arguments as extension at once. only available when def is an array. so we shall ban the -- for '' the ambiguous feature
  * @property {OptKit} [str] External Options to set a string, can escape the quotes
  * @property {OptKit} [num] External Options to set `Number(arg)`. Raise `err` with `NaN`. Provide a JSON Number Type in other language, naturally
- * @property {OptKit} [int] Deprecated External Options to set `BigInt(arg)`. Raise `err` when throw // but since not available in JSON ... and this name confilct with parseInt
- * @property {OptKit} [sym] Deprecated External Options to set `Symbol(arg)`. // but since not available in JSON ... // omg it doesn't even available in most of other languages
+ * @property {OptKit} [nul] Options to set `null`. (what??) maybe the user wants to cause some NullExceptions
+ * @property {OptKit} [rst] Options to set ***def***. The short name is reasonable, so you don't match `reset` when you search `set`, it IS `rst`
  * 
  * @typedef {Object} ListKit now you can put 'set', 'set_set', 'str_num', here. OR add another param to parse() to define how to initialize a var from raw string. YES! It's not my duty! Something like `{ num: v => { if (isNaN(v = Number(v)) throw 'not a number'; return v; } }`, ` { i64: v => { throw 'This is in JAVASCRIPT you Rust guy' } }`, ` { bigint: BigInt } // it throws`. AND throw directly if there's a mistake in `req` as it's a compile time error not a runtime one
  * @property {List} _ ***def***: there is no `Optional<List>` for sure because it's stupid for an emptiable container. All set option in `TextKit` is available to push the value into the array
  * @property {OptKit} [set] Options to set the raw string slice from the argv
- * @property {OptKit} [rst] Options to set the array back to ***def***: `_`
  * @property {OptKit} [str] External Options to set a string, can escape the quotes
  * @property {OptKit} [num] External Options to set `Number(arg)`. Raise `err` with `NaN`. Provide a JSON Number Type in other language, naturally
-
- * @typedef {OptDef | OptDef[]} ExitKit there are only two types of container in JSON: list, dict. And this the list one
- * @typedef {BoolKit | TextKit | ListKit} VarKit
- * @typedef {{ [key: Key]: ExitKit | VarKit }} KitMap The `req` arg of `parse`. `ExitKit` has higher priority (extension is even higher. '--' is highest)
- * @typedef {{ _?: VarVal, [cmd: string]: OptKit }} IVarKit
+ * @property {OptKit} [nul] Options to set `null`, that is, add a `null` to the array. (what??) maybe the user wants to cause some NullExceptions
+ * @property {OptKit} [rst] Options to set the array back to ***def***: `_`
  * 
- * @typedef {Bool | Text | List} VarVal // `undefined` is from `Bool`, `null` is from `Text`
+ * @typedef {Object} PropKit
+ * @property {Prop} _
+ * @property {OptKit} [set] Options to set the raw string slice from the argv. Raise `err` if that is not a `string`
+ * @property {OptKit} [str] External Options to set a string, can escape the quotes. Raise `err` if is not a `string`
+ * @property {OptKit} [num] External Options to set `Number(arg)`. Raise `err` if is not a `number`. Raise `err` with `NaN`. Provide a JSON Number Type in other language, naturally
+ * @property {OptKit} [not] Options to set !***def***. Raise `err` if is not a `boolean`
+ * @property {OptKit} [inv] Options to set `!cur`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [yes] Options to set `true`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [no]  Options to set `false`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [on]  Options to set `true`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [off] Options to set `false`. Raise `err` if is not a `boolean`
+ * @property {OptKit} [nul] Options to set `null`, or add a `null` to the array.
+ * @property {OptKit} [rst] Options to set the value/array in that postion back to ***def***: `_`
+ * 
+ * @typedef {OptDef | OptDef[]} ExitKit there are only two types of container in JSON: list, dict. And this the list one
+ * @typedef {BoolKit | TextKit | ListKit | PropKit} VarKit
+ * @typedef {{ _?: VarVal, [act: string]: OptKit }} VarKitLike
+ * @typedef {{ [key: Key]: ExitKit | VarKit }} KitMap The `req` arg of `parse`. `ExitKit` has higher priority (extension is even higher. '--' is highest)
+ * 
+ * @typedef {Bool | Text | List | Prop} VarVal // `undefined` is from `Bool`, `null` is from `Text`
  * @typedef {{ [key: Key]: VarVal }} VarValMap The `req` arg of `parse`.
  * @typedef {{ i: number, key: Key, opt: Opt }} ExitVal 
  * 
  * @callback IsFatal
- * @param {{ i: number, msg: string, opt: Opt, key?: Key, val?: VarVal }} err
+ * @param {{ i: number, msg: string, opt: Opt, key?: Key, val?: VarVal, prop?: Prop }} err
  * @returns {boolean} Whether the parsing should continue (false) or quit (true)
  * @typedef {(k: Key, v: VarVal) => void} Act
- * @typedef {{ [cmd: string]: Act }} KitImpl
+ * @typedef {{ [act: string]: Act }} KitImpl
  * @typedef {{ [opt: Opt]: { a: Act, k: Key } }} OptReg internal type
  */
+const isA = Array.isArray;
 /** Get OD! @param {OptKit} ok */
 const god = ok => ok === undefined ? [] : isA(ok) ? ok : [ok];
 export const Config = {
